9b99d28657b00c0b150818b84a5c64ecc3018696
tylko_beatrycze <beata.szczygielska@gmail.com>
2017-07-25 13:38:51 +0200

    Multiple svg tags replaced by single one

--- src/App.tsx
+++ src/App.tsx
@@ -13,29 +13,27 @@ class App extends React.Component<{}, {}> {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        {/* <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p> */}
         <div className="Graph-container">
           <h3 className="Graph-header">Nomination Tool</h3>
-            <svg width="150" height={graph1.value * 100} viewBox="0 0 100 200">
-              <rect x="10" y="0" width="100" height="200" fill="#3c66e8" />
-              <text x="50%" y="190" text-anchor="middle">{graph1.value}</text>
-            </svg>
-            <svg width="150" height={graph2.value * 100} viewBox="0 0 100 300">
-              <rect x="10" y="0" width="100" height="300" fill="#3c66e8" />
-              <text x="50%" y="290" text-anchor="middle">{graph2.value}</text>
-            </svg>
-            <svg width="150" height={graph3.value * 100} viewBox="0 0 100 100">
-              <rect x="10" y="0" width="100" height="100" fill="#3c66e8" />
-              <text x="50%" y="90" text-anchor="middle">{graph3.value}</text>
-            </svg>
-            <svg width="500" height="5" viewBox="0 0 500 5">
-              <line x1="0" y1="0" x2="500" y2="0"
+          <svg width="500" height="390">
+              <rect x="50" y={350 - graph1.value * 100} width="100" height={graph1.value * 100} fill="#3c66e8" />
+              <text x="19%" y="330" text-anchor="middle">{graph1.value}</text>
+              <text x="17%" y="380" text-anchor="middle">{graph1.label}</text>
+
+              <rect x="200" y={350 - graph2.value * 100} width="100" height={graph2.value * 100} fill="#3c66e8" />
+              <text x="49%" y="330" text-anchor="middle">{graph2.value}</text>
+              <text x="45%" y="380" text-anchor="middle">{graph2.label}</text>
+
+              <rect x="350" y={350 - graph3.value * 100} width="100" height={graph3.value * 100} fill="#3c66e8" />
+              <text x="79%" y="330" text-anchor="middle">{graph3.value}</text>
+              <text x="77%" y="380" text-anchor="middle">{graph3.label}</text>
+
+              <line x1="0" y1="350" x2="500" y2="350"
                 stroke-width="0.2" stroke="#878383"/>
-            </svg>
-            <div>
-              <div className="Graph-label">{graph1.label}</div>
-              <div className="Graph-label">{graph2.label}</div>
-              <div className="Graph-label">{graph3.label}</div>
-            </div>
+          </svg>
         </div>
       </div>
     );
