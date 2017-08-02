157063e0e3ca2d1e17c8b58e1e9ef0b57b815e78
tylko_beatrycze <beata.szczygielska@gmail.com>
2017-08-02 10:35:33 +0200

    Indentation refactored

--- src/chart/chart-size.ts
+++ src/chart/chart-size.ts
@@ -1,26 +1,26 @@
 import { Column } from './column';
 
 interface ContainerSize { 
-    height: number;
-    width: number;
-    spacer: number;
+  height: number;
+  width: number;
+  spacer: number;
 }
 
 export function getChartSize(container: ContainerSize, graphs: Column[]) {
-    const maxRect: Column = graphs.reduce(
-        (prev: Column, current: Column) => (prev.value > current.value) ? prev : current
-    );
-    const heightCalc: number = (container.height - container.spacer / 2) / maxRect.value;
-    const widthCalc: number = (container.width - container.spacer) / graphs.length;
-    const svgHeight: number = heightCalc * maxRect.value;
-    const svgWidth: number = widthCalc * graphs.length;
+  const maxRect: Column = graphs.reduce(
+      (prev: Column, current: Column) => (prev.value > current.value) ? prev : current
+  );
+  const heightCalc: number = (container.height - container.spacer / 2) / maxRect.value;
+  const widthCalc: number = (container.width - container.spacer) / graphs.length;
+  const svgHeight: number = heightCalc * maxRect.value;
+  const svgWidth: number = widthCalc * graphs.length;
 
-    return {
-        heightCalc: heightCalc,
-        widthCalc: widthCalc,
-        svg: {
-            height: svgHeight,
-            width: svgWidth
-        }
-    };
+  return {
+      heightCalc: heightCalc,
+      widthCalc: widthCalc,
+      svg: {
+          height: svgHeight,
+          width: svgWidth
+      }
+  };
 }
--- src/fake/data.ts
+++ src/fake/data.ts
@@ -1,44 +1,44 @@
 import { Column } from '../chart/column';
 
 export const staticData: Column[] = [
-    {
-        value: 20,
-        label: 'Top'
-    },
-    {
-        value: 3,
-        label: 'Good'
-    },
-    {
-        value: 1,
-        label: 'Low'
-    },
-    {
-        value: 4,
-        label: 'New'
-    },
-    {
-        value: 5,
-        label: 'Next'
-    },
-    {
-        value: 10,
-        label: 'Next'
-    },
-    {
-        value: 2,
-        label: 'Next'
-    },
-    {
-        value: 9,
-        label: 'Next'
-    },
-    {
-        value: 16,
-        label: 'Next'
-    },
-    {
-        value: 8,
-        label: 'Next'
-    }
+  {
+      value: 20,
+      label: 'Top'
+  },
+  {
+      value: 3,
+      label: 'Good'
+  },
+  {
+      value: 1,
+      label: 'Low'
+  },
+  {
+      value: 4,
+      label: 'New'
+  },
+  {
+      value: 5,
+      label: 'Next'
+  },
+  {
+      value: 10,
+      label: 'Next'
+  },
+  {
+      value: 2,
+      label: 'Next'
+  },
+  {
+      value: 9,
+      label: 'Next'
+  },
+  {
+      value: 16,
+      label: 'Next'
+  },
+  {
+      value: 8,
+      label: 'Next'
+  }
 ];
