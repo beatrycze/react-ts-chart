e610d555ca2aad6f6ee6400e09c8d1697dfa2e05
Beata Szczygielska <beata.szczygielska@gmail.com>
2017-08-03 12:27:01 +0200

    ref added (test)

--- src/components/TestContainer.tsx
+++ src/components/TestContainer.tsx
@@ -9,11 +9,22 @@ interface Props {
 }
 
 class TestContainerComponent extends React.Component<Props, {}> {
+  private el: any;
+
+  handleSomething() {
+    console.log(this.el);
+  }
+
   render() {
     // console.log(this);
+    // let refs = this.refs;
+    // console.log(refs);
     return (
       <div className="Test-container">
-        <Circle rCircle={this.props.width / 4}/> {/* TODO Passing current (depeded on screen size) width from <div>. How? */}
+        <Circle 
+          rCircle={this.props.width / 4}
+          ref={(element) => { this.el = element; }}
+        /> {/* TODO Passing current (depeded on screen size) width from <div>. How? */}
         {this.props.message}
       </div>
     );
